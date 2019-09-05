b346106ce86b79d1e6228de71ce4203b35e79e93
matsumana07384 <manami.m.227@outlook.jp>
2019-09-05 17:57:57 +0900

    work on 20190905

--- src/views/Tag002.vue
+++ src/views/Tag002.vue
@@ -1,29 +1,59 @@
 <template>
 <div>
     <h1>HTML基礎講座vol.3 〜タグ基本編〜</h1>
+
     <div class="block">
         <p>divタグ</p>
         <div class="mybox">div</div>
     </div>
+
     <div class="block">
         <p>articleタグ</p>
         <article class="mybox">article</article>
     </div>
+
     <section class="block">
         <h3>タグ</h3>
         <section class="mybox">section</section>
     </section>
+
     <div>
         <p>navタグとul、liタグ</p>
         <nav>
             <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
+                <li><a href="https://google.com" target="_blank">1</a></li>
+                <li><a href="https://yahoo.co.jp" target="_blank">2</a></li>
+                <li><router-link to="/">3</router-link></li>
             </ul>
         </nav>        
     </div>
-<!-- aside
+    
+    <div>
+        <p>asideタグ</p>
+        <aside>
+            <p>asideで囲まれた要素</p>
+        </aside>
+        <p>本文</p>
+    </div>
+
+    <div>
+        <p>h1〜h6タグ</p>
+        <hgroup>
+            <h1>title</h1>
+            <h2>title</h2>
+            <h3>title</h3>
+            <h4>title</h4>
+            <h5>title</h5>
+            <h6>title</h6>
+        </hgroup>
+    </div>
+
+    <main>
+        
+    </main>
+
+
+<!-- 
 
 main
 
