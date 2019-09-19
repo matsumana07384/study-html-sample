1639f171c86f4aff432f1ba5900b6ea0cd856b99
matsumana07384 <manami.m.227@outlook.jp>
2019-09-19 11:22:30 +0900

    wip:input attribute

--- src/views/Input.vue
+++ src/views/Input.vue
@@ -1,4 +1,47 @@
 <template lang="pug">
+div
+    section
+        h1 Input要素のtype属性
+        div
+            form
+            div
+                p color
+                label カラーパレッド：
+                    input(type='color', value='#')
+            div
+                p checkbox
+                    label 
+                        input(type='checkbox',value='小麦粉')
+                        |小麦粉
+                    label
+                        input(type='checkbox',value='ベーキングパウダー')
+                        |ベーキングパウダー
+                    label
+                        input(type='checkbox',value='砂糖')
+                        |砂糖
+            p date
+            p datetime-local
+            p email
+            p file
+            p hidden
+            p radio
+            p range
+            label
+                input.form-control(v-model='range', type='range', min='0', max='10')
+            p reset
+            p password
+            input(type='password', maxlength='8', placeholder='パスワード入力')
+            p serch
+            p submit
+            p tel
+            p text
+            label
+                | テキスト：
+                input(type='text', name='text', value='未入力', placeholder='テキスト')
+            p url
+    hr
+    section
+        h2 その他属性
 
 </template>
 <script>
